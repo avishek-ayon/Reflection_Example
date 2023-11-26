fbe3a0ada466f525d7bc68e30dab9df59ae21c81
Avishek <avishek.palit96@gmail.com>
2023-11-26 14:46:12 +0600

    Object to Json Using Reflection and Recursion

---
+++ src/Reflection_Example/Reflection_Example/JsonFormatter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    public static class JsonFormatter
+    {
+        public static string Convert(object item)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append("{");
+
+            Type type = item.GetType();
+            PropertyInfo[] propertiesInfo = type.GetProperties();
+            int propertyLength = propertiesInfo.Length;
+            int initialProperty = 0;
+
+            foreach (PropertyInfo property in propertiesInfo)
+            {
+                object propertyValue = property.GetValue(item);
+                Type propertyType = property.PropertyType;
+            
+
+                stringBuilder.Append($"\" { property.Name} \":");
+                if (propertyType == typeof(int) || propertyType == typeof(double) || propertyType == typeof(float) || propertyType == typeof(decimal) || propertyType == typeof(sbyte) || propertyType == typeof(long) || propertyType == typeof(short) || propertyType == typeof(byte))
+                {
+                    stringBuilder.Append($"{propertyValue.ToString()}");
+                }
+                else if (propertyType.IsPrimitive || propertyType == typeof(string) || propertyType == typeof(DateTime))
+                {
+                    stringBuilder.Append($"\" {propertyValue.ToString()} \"");
+                }
+                else if (typeof(IEnumerable).IsAssignableFrom(propertyType))
+                {
+                    stringBuilder.Append("[");
+                    IEnumerable enumerable = (IEnumerable)propertyValue;
+                    int currentItem = 0;
+                    int itemCount = enumerable.Cast<object>().Count();
+
+                    foreach (object prop in enumerable)
+                    {
+                        stringBuilder.Append(Convert(prop));
+                        if (currentItem < itemCount - 1)
+                        {
+                            stringBuilder.Append(",");
+                        }
+                        currentItem++;
+                    }
+
+                    stringBuilder.Append("]");
+                }
+                else
+                {
+                    stringBuilder.Append(Convert(propertyValue));
+                }
+
+                if (initialProperty < propertyLength - 1)
+                {
+                    stringBuilder.Append(",");
+                }
+                initialProperty++;
+            }
+
+            stringBuilder.Append("}");
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ src/Reflection_Example/Reflection_Example/Program.cs
@@ -0,0 +1,65 @@
+﻿
+using Assignment1;
+
+Course course = new Course
+{
+    Title = "Asp.net",
+    Teacher = new Instructor
+    {
+        Name = "Ayon",
+        Email = "Avkd@",
+        PresentAddress = new Address
+        {
+            Street = "bbd",
+            City = "Ctg",
+            Country = "Ban"
+        },
+        PermanentAddress = new Address
+        {
+            Street = "bbd",
+            City = "Ctg",
+            Country = "Ban"
+        },
+        PhoneNumbers = new List<Phone>
+            {
+                new Phone
+                {
+                    Number = "0000000",
+                    Extension = "018",
+                    CountryCode = "114"
+                }
+            }
+    },
+
+    Topics = new List<Topic>
+    {
+        new Topic
+        {
+            Title = "Installation",
+            Description = "Later Give on",
+            Sessions = new List<Session>
+             {
+               new Session
+               {
+                  DurationInHour = 2,
+                  LearningObjective = "By Starting Professional Careerer"
+               }
+            }
+        }
+
+    },
+    Fees = 30000,
+    Tests = new List<AdmissionTest>
+    {
+      new AdmissionTest
+      {
+          StartDateTime = DateTime.Now,
+          EndDateTime = DateTime.Now,
+          TestFees = 100
+      }
+    }
+
+};
+
+string json = JsonFormatter.Convert(course);
+Console.WriteLine(json);
